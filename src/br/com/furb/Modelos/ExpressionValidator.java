839c8066b0e5467f8663170f9083afa84080b8bd
Gustavo Merini Seibt <gutomerini@gmail.com>
2018-03-15 00:00:15 -0300

    Adicionando mais regex, metodo toString()/main para testes

--- src/br/com/furb/Modelos/ExpressionValidator.java
+++ src/br/com/furb/Modelos/ExpressionValidator.java
@@ -7,100 +7,138 @@ package br.com.furb.model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import br.com.furb.model.EnumType;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author gseibt
  */
 public class ExpressionValidator {
-    
-    private String word;
-    private EnumType type;
+
+	@Override
+	public String toString() {
+		return "ExpressionValidator [qtyEngine=" + qtyEngine + ", qtyFuel=" + qtyFuel + ", qtyValue=" + qtyValue
+				+ ", qtyKM=" + qtyKM + ", qtyYear=" + qtyYear + "]";
+	}
+	//private String word;
+	private static final String kmRegex = "[0-9]{1,3}\\.[0-9]{3}|[0-9]{1,3}"; //[0-9]{2,3}\.[0-9]{3}|[0-9]{1}\.[0-9]{3}|[0-9]{1,3}
+	private static final String yearRegex = "\\d{4}";
+	private static final String engineRegex = "[1-9]['.'][0-9]";
+	private static final String fuelRegex = "¡lcool|BiocombustÌvel|Diesel|Gasolina";
+	private static final String symbolRegex = "^([¡]|[B]|[D]|[G]|[R])";
+
     private int qtyEngine;
     private int qtyFuel;
     private int qtyValue;
     private int qtyKM;
+	private int qtyYear;
 
-    /**
-     * @return the word
-     */
-    public String getword() {
-        return word;
-    }
-
-    /**
-     * @param word the word to set
-     */
-    public void setword(String word) {
-        this.word = word;
-    }
+//    /**
+//     * @return the word
+//     */
+//    public String getword() {
+//        return word;
+//    }
+//
+//    /**
+//     * @param word the word to set
+//     */
+//    public void setword(String word) {
+//        this.word = word;
+//    }
    
-    /**
-     * @return the type
-     */
-    public EnumType getType() {
-        return type;
-    }
 
-    /**
-     * @param type the type to set
-     */
-    public void setType(EnumType type) {
-        this.type = type;
-    }    
-    
+
     public boolean validateword(String word) {
         return true;
     }
-    public void process(String strTextArea) {
+    public String process(String strTextArea) {
     	int currentLine = 1;
     	for (String line : strTextArea.split("\r\n")) {
     		for (String word : line.split("\\s")) {
-    			validateNumber(word);
-    			validateSymbol(word);
+    			if(Character.isDigit(word.charAt(0))) { 
+    				if (!validateNumber(word)) {
+    					throw new IllegalArgumentException("KM, ano ou motor inv·lido na linha " + currentLine);
+    				} else {
+    					// identifica oq È
+    					
+    				}
+    			} else {
+    				if (!validateSymbol(word)) {
+    					throw new IllegalArgumentException("Simbolo inv·lido na linha " + currentLine);
+    				}    				
+    			}
+    			
 			}
+    		
     		currentLine++;
 		}
+    	
+    	return this.toString();
+    	
     }
     
-    private boolean validateNumber(String word) {
-    /*
-     * qualquer palavra que inicia com digito e n√£o segue o padr√£o de forma√ß√£o especificado para Montor, Ano ou KM √© inv√°lido    
-     */
-        return true;
+    boolean validateNumber(String word) {
+    	Matcher matcher;
+    	matcher = Pattern.compile(yearRegex).matcher(word);
+    	if (matcher.find()) {
+    		setQtyYear(getQtyYear() + 1);
+    		return true;
+    	}
+    	matcher = Pattern.compile(engineRegex).matcher(word);
+    	if(matcher.find()) {
+    		qtyEngine++;
+    		return true;
+    	}
+    	matcher = Pattern.compile(kmRegex).matcher(word);
+    	if(matcher.find()) {
+    		if(Integer.parseInt(word.replaceAll("\\.", "")) <= 200000) {
+    			qtyKM++;
+    			return true;
+    		} 
+		} 
+    		
+		return false;
     }
     
     private boolean validateSymbol(String word) {
-        return true;
-    }
-    
-    private EnumType identifyType(String word) {
-        return this.getType();
+    	boolean result = true;
+    	Matcher matcher;
+    	matcher = Pattern.compile(symbolRegex).matcher(word);    	
+    	if (!matcher.find()) {
+    		result = false;
+    	} 
+    	return result;
     }
     
     private void identifyIsEngine(String word) {
         /* palavra composta por um digito 1..9, seguido de ponto, seguido de um digito
         * ex: 1.4 
         */
-        this.setType(EnumType.Engine);
+  
     }
     
     private void identifyIsValue(String word) {
         /*
         * EX: R$200,00
         */
-        this.setType(EnumType.Value);
+
     }
     
     private void identifyIsFuel(String word) {
-        this.setType(EnumType.Value);
+
     }
     
     private void identifyIsKM(String word) {
-        this.setType(EnumType.KM);
-    }    
+
+    }
+	public int getQtyYear() {
+		return qtyYear;
+	}
+	public void setQtyYear(int qtyYear) {
+		this.qtyYear = qtyYear;
+	}    
 
     
     
