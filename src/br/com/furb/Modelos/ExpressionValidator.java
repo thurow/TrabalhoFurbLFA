55ab27701632493800a31348d0774044f0b26ba4
Gustavo Merini Seibt <gutomerini@gmail.com>
2018-03-15 20:50:31 -0300

    bkp

--- src/br/com/furb/Modelos/ExpressionValidator.java
+++ src/br/com/furb/Modelos/ExpressionValidator.java
@@ -5,8 +5,11 @@
  */
 package br.com.furb.model;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,125 +24,129 @@ public class ExpressionValidator {
 		return "ExpressionValidator [qtyEngine=" + qtyEngine + ", qtyFuel=" + qtyFuel + ", qtyValue=" + qtyValue
 				+ ", qtyKM=" + qtyKM + ", qtyYear=" + qtyYear + "]";
 	}
-	//private String word;
-	private static final String kmRegex = "[0-9]{1,3}\\.[0-9]{3}|[0-9]{1,3}"; //[0-9]{2,3}\.[0-9]{3}|[0-9]{1}\.[0-9]{3}|[0-9]{1,3}
+
+	// private String word;
+	private static final String kmRegex = "[0-9]{1,3}\\.[0-9]{3}|[0-9]{1,3}"; // [0-9]{2,3}\.[0-9]{3}|[0-9]{1}\.[0-9]{3}|[0-9]{1,3}
 	private static final String yearRegex = "\\d{4}";
 	private static final String engineRegex = "[1-9]['.'][0-9]";
 	private static final String fuelRegex = "Álcool|Biocombustível|Diesel|Gasolina";
 	private static final String symbolRegex = "^([Á]|[B]|[D]|[G]|[R])";
-
-    private int qtyEngine;
-    private int qtyFuel;
-    private int qtyValue;
-    private int qtyKM;
+	private static final String valueRegex = "(R\\$[0-9]{1,3}\\.[0-9]{3}\\,[0-9]{2}|R\\$[0-9]{1,3}\\,[0-9]{2})";/// "(R\\$[0-9]{1,3}\\.[0-9]{3}\\,[0-9]{2})";
+	private static final String invalidValueRegex = "(R\\$[0]{2})";
+	private int qtyEngine;
+	private int qtyFuel;
+	private int qtyValue;
+	private int qtyKM;
 	private int qtyYear;
+	private Matcher matcher;
 
-//    /**
-//     * @return the word
-//     */
-//    public String getword() {
-//        return word;
-//    }
-//
-//    /**
-//     * @param word the word to set
-//     */
-//    public void setword(String word) {
-//        this.word = word;
-//    }
-   
+	public int getQtyEngine() {
+		return qtyEngine;
+	}
 
+	public void setQtyEngine(int qtyEngine) {
+		this.qtyEngine = qtyEngine;
+	}
 
-    public boolean validateword(String word) {
-        return true;
-    }
-    public String process(String strTextArea) {
-    	int currentLine = 1;
-    	for (String line : strTextArea.split("\r\n")) {
-    		for (String word : line.split("\\s")) {
-    			if(Character.isDigit(word.charAt(0))) { 
-    				if (!validateNumber(word)) {
-    					throw new IllegalArgumentException("KM, ano ou motor inválido na linha " + currentLine);
-    				} else {
-    					// identifica oq é
-    					
-    				}
-    			} else {
-    				if (!validateSymbol(word)) {
-    					throw new IllegalArgumentException("Simbolo inválido na linha " + currentLine);
-    				}    				
-    			}
-    			
-			}
-    		
-    		currentLine++;
-		}
-    	
-    	return this.toString();
-    	
-    }
-    
-    boolean validateNumber(String word) {
-    	Matcher matcher;
-    	matcher = Pattern.compile(yearRegex).matcher(word);
-    	if (matcher.find()) {
-    		setQtyYear(getQtyYear() + 1);
-    		return true;
-    	}
-    	matcher = Pattern.compile(engineRegex).matcher(word);
-    	if(matcher.find()) {
-    		qtyEngine++;
-    		return true;
-    	}
-    	matcher = Pattern.compile(kmRegex).matcher(word);
-    	if(matcher.find()) {
-    		if(Integer.parseInt(word.replaceAll("\\.", "")) <= 200000) {
-    			qtyKM++;
-    			return true;
-    		} 
-		} 
-    		
-		return false;
-    }
-    
-    private boolean validateSymbol(String word) {
-    	boolean result = true;
-    	Matcher matcher;
-    	matcher = Pattern.compile(symbolRegex).matcher(word);    	
-    	if (!matcher.find()) {
-    		result = false;
-    	} 
-    	return result;
-    }
-    
-    private void identifyIsEngine(String word) {
-        /* palavra composta por um digito 1..9, seguido de ponto, seguido de um digito
-        * ex: 1.4 
-        */
-  
-    }
-    
-    private void identifyIsValue(String word) {
-        /*
-        * EX: R$200,00
-        */
+	public int getQtyFuel() {
+		return qtyFuel;
+	}
 
-    }
-    
-    private void identifyIsFuel(String word) {
+	public void setQtyFuel(int qtyFuel) {
+		this.qtyFuel = qtyFuel;
+	}
 
-    }
-    
-    private void identifyIsKM(String word) {
+	public int getQtyValue() {
+		return qtyValue;
+	}
 
-    }
+	public void setQtyValue(int qtyValue) {
+		this.qtyValue = qtyValue;
+	}
+
+	public int getQtyKM() {
+		return qtyKM;
+	}
+
+	public void setQtyKM(int qtyKM) {
+		this.qtyKM = qtyKM;
+	}
+
 	public int getQtyYear() {
 		return qtyYear;
 	}
+
 	public void setQtyYear(int qtyYear) {
 		this.qtyYear = qtyYear;
-	}    
+	}
 
-    
-    
+	public String process(String strTextArea) {
+		int currentLine = 1;
+		for (String line : strTextArea.split("\r\n")) {
+			for (String word : line.split("\\s")) {
+				if (Character.isDigit(word.charAt(0))) {
+					if (!validateNumber(word)) {
+						throw new IllegalArgumentException("KM, ano ou motor inválido na linha " + currentLine);
+					} else {
+						if (!validateSymbol(word)) {
+							throw new IllegalArgumentException("Simbolo inválido na linha " + currentLine);
+						}
+					}
+
+				}
+
+				currentLine++;
+			}
+		}
+
+		return this.toString();
+
+	}
+
+	public boolean validateNumber(String word) {
+		Matcher matcher;
+		matcher = Pattern.compile(yearRegex).matcher(word);
+		if (matcher.find()) {
+			qtyYear++;
+			return true;
+		}
+		matcher = Pattern.compile(engineRegex).matcher(word);
+		if (matcher.find()) {
+			qtyEngine++;
+			return true;
+		}
+		matcher = Pattern.compile(kmRegex).matcher(word);
+		if (matcher.find()) {
+			if (Integer.parseInt(word.replaceAll("\\.", "")) <= 200000) {
+				qtyKM++;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean validateSymbol(String word) {
+
+		matcher = Pattern.compile(symbolRegex).matcher(word);
+		if (!matcher.find()) {
+			return false;
+		}
+		matcher = Pattern.compile(fuelRegex).matcher(word);
+		if (matcher.find()) {
+			qtyFuel++;
+			return true;
+		}
+		matcher = Pattern.compile(valueRegex).matcher(word);
+		if (matcher.find()) {
+			matcher = Pattern.compile(invalidValueRegex).matcher(word);
+			if (matcher.find()) {
+				return false;
+			}
+			qtyValue++;
+			return true;
+		}
+		return false;
+	}
+
 }
