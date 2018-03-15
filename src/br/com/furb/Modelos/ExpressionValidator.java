5bfe1865de18238161626c7e235f9bd6ea4e5426
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2018-03-15 19:21:28 -0300

    correções gerais e de indentação

--- src/br/com/furb/Modelos/ExpressionValidator.java
+++ src/br/com/furb/Modelos/ExpressionValidator.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.furb.model;
+package br.com.furb.Modelos;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,8 +25,8 @@ public class ExpressionValidator {
 	private static final String kmRegex = "[0-9]{1,3}\\.[0-9]{3}|[0-9]{1,3}"; //[0-9]{2,3}\.[0-9]{3}|[0-9]{1}\.[0-9]{3}|[0-9]{1,3}
 	private static final String yearRegex = "\\d{4}";
 	private static final String engineRegex = "[1-9]['.'][0-9]";
-	private static final String fuelRegex = "Álcool|Biocombustível|Diesel|Gasolina";
-	private static final String symbolRegex = "^([Á]|[B]|[D]|[G]|[R])";
+	private static final String fuelRegex = "ï¿½lcool|Biocombustï¿½vel|Diesel|Gasolina";
+	private static final String symbolRegex = "^([ï¿½]|[B]|[D]|[G]|[R])";
 
     private int qtyEngine;
     private int qtyFuel;
@@ -47,39 +47,37 @@ public class ExpressionValidator {
 //    public void setword(String word) {
 //        this.word = word;
 //    }
-   
+
 
 
     public boolean validateword(String word) {
         return true;
     }
     public String process(String strTextArea) {
-    	int currentLine = 1;
-    	for (String line : strTextArea.split("\r\n")) {
-    		for (String word : line.split("\\s")) {
-    			if(Character.isDigit(word.charAt(0))) { 
-    				if (!validateNumber(word)) {
-    					throw new IllegalArgumentException("KM, ano ou motor inválido na linha " + currentLine);
-    				} else {
-    					// identifica oq é
-    					
-    				}
-    			} else {
-    				if (!validateSymbol(word)) {
-    					throw new IllegalArgumentException("Simbolo inválido na linha " + currentLine);
-    				}    				
-    			}
-    			
-			}
-    		
-    		currentLine++;
-		}
-    	
-    	return this.toString();
-    	
+        int currentLine = 1;
+        for (String line : strTextArea.split("\r\n")) {
+            for (String word : line.split("\\s")) {
+                if(Character.isDigit(word.charAt(0))) {
+                    if (!validateNumber(word)) {
+                            throw new IllegalArgumentException("KM, ano ou motor invï¿½lido na linha " + currentLine);
+                    } else {
+                            // identifica oq ï¿½
+
+                    }
+                } else {
+                    if (!validateSymbol(word)) {
+                            throw new IllegalArgumentException("Simbolo invï¿½lido na linha " + currentLine);
+                    }
+                }
+
+            }
+            currentLine++;
+        }
+        return this.toString();
+
     }
-    
-    boolean validateNumber(String word) {
+
+    public boolean validateNumber(String word) {
     	Matcher matcher;
     	matcher = Pattern.compile(yearRegex).matcher(word);
     	if (matcher.find()) {
@@ -93,53 +91,52 @@ public class ExpressionValidator {
     	}
     	matcher = Pattern.compile(kmRegex).matcher(word);
     	if(matcher.find()) {
-    		if(Integer.parseInt(word.replaceAll("\\.", "")) <= 200000) {
-    			qtyKM++;
-    			return true;
-    		} 
-		} 
-    		
-		return false;
+            if(Integer.parseInt(word.replaceAll("\\.", "")) <= 200000) {
+                    qtyKM++;
+                    return true;
+            }
+        }
+        return false;
     }
-    
+
     private boolean validateSymbol(String word) {
     	boolean result = true;
     	Matcher matcher;
-    	matcher = Pattern.compile(symbolRegex).matcher(word);    	
+    	matcher = Pattern.compile(symbolRegex).matcher(word);
     	if (!matcher.find()) {
     		result = false;
-    	} 
+    	}
     	return result;
     }
-    
+
     private void identifyIsEngine(String word) {
         /* palavra composta por um digito 1..9, seguido de ponto, seguido de um digito
-        * ex: 1.4 
+        * ex: 1.4
         */
-  
+
     }
-    
+
     private void identifyIsValue(String word) {
         /*
         * EX: R$200,00
         */
 
     }
-    
+
     private void identifyIsFuel(String word) {
 
     }
-    
+
     private void identifyIsKM(String word) {
 
     }
-	public int getQtyYear() {
-		return qtyYear;
-	}
-	public void setQtyYear(int qtyYear) {
-		this.qtyYear = qtyYear;
-	}    
+    public int getQtyYear() {
+            return qtyYear;
+    }
+    public void setQtyYear(int qtyYear) {
+            this.qtyYear = qtyYear;
+    }
 
-    
-    
+
+
 }
